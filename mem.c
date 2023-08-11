084ce8336410fe3be26b8b7fad36958bf596e677
RatchanonWeb <ratchanonweb46@gmail.com>
2023-08-11 22:27:12 +0700

    show heap and stack address

--- mem.c
+++ mem.c
@@ -9,15 +9,18 @@ void call_stack(int n) {
     else {
         int local_var = 5;
         printf("stack address of local var is %p.\n",&local_var);
+        value = (int *)malloc(sizeof(int) * n);      
+        printf("heap address is %p.\n",value);
+        printf("\n");
     }
-    value = (int *)malloc(sizeof(int) * n);      
     call_stack(n-1);
 }
 
 int main() {
     call_stack(3);
-    printf("you can see the more local var, the less stack address.");
-
+    printf("You can see the more local var, the less stack address.\n");
+    printf("And you can also see the more malloc, the more heap address.\n");
+    free(value);
     return 0;
 }
 
