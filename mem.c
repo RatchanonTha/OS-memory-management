23c820ad8fbc861735f224b0b4e8301e6ab7f92b
RatchanonWeb <ratchanonweb46@gmail.com>
2023-08-11 22:35:23 +0700

    add global var

--- mem.c
+++ mem.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int *value;
+int a,b,c;
 void call_stack(int n) {
     if (n==0) {
         return;
@@ -10,7 +11,7 @@ void call_stack(int n) {
         int local_var = 5;
         printf("stack address of local var is %p.\n",&local_var);
         value = (int *)malloc(sizeof(int) * n);      
-        printf("heap address is %p.\n",value);
+        printf("heap address from malloc is %p.\n",value);
         printf("\n");
     }
     call_stack(n-1);
@@ -18,9 +19,13 @@ void call_stack(int n) {
 
 int main() {
     call_stack(3);
+    printf("heap address of a is %p.\n",&a);
+    printf("heap address of b is %p.\n",&b);
+    printf("heap address of c is %p.\n",&c);
     printf("You can see the more local var, the less stack address.\n");
-    printf("And you can also see the more malloc, the more heap address.\n");
+    printf("And you can also see the more malloc and the more global var, the more heap address.\n");
     free(value);
+
     return 0;
 }
 
